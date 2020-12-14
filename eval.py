5f2c2b4986df7b5fff63831a56d5574d60d94650
Robert <b96201015@gmail.com>
2020-12-14 17:15:41 +0800

    submission int

--- eval.py
+++ eval.py
@@ -32,10 +32,6 @@ for i, im_path in enumerate(test_images):
     # Make submission
     predictions.append((os.path.splitext(im_path.split('/')[-1])[0], output.item()))
 
-    if i == 10:
-        break
-
-
 submission = pd.DataFrame(predictions)
 submission.columns = ['id_code','diagnosis']
 submission.to_csv('submission_raw_value.csv', index=False)
@@ -46,7 +42,6 @@ submission.loc[(0.7 <= submission.diagnosis) & (submission.diagnosis < 1.5), 'di
 submission.loc[(1.5 <= submission.diagnosis) & (submission.diagnosis < 2.5), 'diagnosis'] = 2
 submission.loc[(2.5 <= submission.diagnosis) & (submission.diagnosis < 3.5), 'diagnosis'] = 3
 submission.loc[3.5 <= submission.diagnosis, 'diagnosis'] = 4
-
 submission['diagnosis'] = submission['diagnosis'].astype(int)
 submission.to_csv('submission.csv', index=False)
 submission.head()
